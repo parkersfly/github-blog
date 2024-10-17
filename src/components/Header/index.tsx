27db02ad6f6916991a00006487b31aa767c23df5
parker <parkersfly@gmail.com>
2024-10-17 12:55:20 -0300

    Add component Header - Structure and styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
-import { HeaderContainer } from "./styles";
+import { HeaderContainer, HeaderContent } from "./styles";
+import logo from '../../assets/logo.svg'
 
 export function Header(){
   return (
     <HeaderContainer>
-      
+      <HeaderContent>
+        <img src={logo} alt="" />
+      </HeaderContent>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,19 @@
 import styled from "styled-components";
+import cover from '../../assets/cover.png'
 
 export const HeaderContainer = styled.header`
-  
+  width: 100%;
+  height: 29.6rem;
+
+  padding-top: 5.7rem;
+
+  background-image: url(${cover});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`
+
+export const HeaderContent = styled.div`
+  display: flex;
+  justify-content: center;
 `
